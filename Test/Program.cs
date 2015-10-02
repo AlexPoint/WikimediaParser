caa92d694f3b7194b8139419bbdd90500968b8c3
Alexandre Point <apoint@fleex.tv>
2015-10-02 12:44:34 +0200

    Cleanup: renamed wiktionary to wikimedia
    The parser can be used for all wikimedia projects
    (wikipedia, wiktionary etc.)

--- Test/Program.cs
+++ Test/Program.cs
@@ -25,7 +25,7 @@ namespace Test
         static void Main(string[] args)
         {
             var dictionaryBuilder = new DictionaryBuilder();
-            dictionaryBuilder.CreateDictionary("en", "fr", Wikimedia.Wikipedia);
+            dictionaryBuilder.CreateDictionary("en", "ar", Wikimedia.Wikipedia);
 
             Console.WriteLine("======= END ========");
             Console.ReadKey();
