d1181f1ae0bfb20be8cb6945bb80194be1f4e73d
Alex <pointalexandre@gmail.com>
2019-05-19 15:37:45 +0200

    Setup ETL project for ETL operations
    Instead of using complex tool, we code the data manipulations in C# instead to test the waters.
    It may not be 100% automated but it provides greater control on the data transformation and the change tracking (via git).
    Note: we target .netcore for this project because the ETLBox nuget package target this framework as well and also because it should be deployable more easily later (than the .net framework)

---
+++ ETL/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.IO;
+
+namespace ETL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json");
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            Console.WriteLine(config["connectionString"]);
+        }
+    }
+}
